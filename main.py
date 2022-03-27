c680dcf64b0ce074a597c0579a93b8cbce4aada7
ferolivares98 <ferolinaranjo@gmail.com>
2022-03-27 03:58:48 +0200

    Uncontrollable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import pygame
-
 from constants import *
 from robot import *
 
@@ -15,9 +14,13 @@ def main():
                                       FULL_MAP_HEIGHT))
     pygame.display.set_caption(' Robot  |  Odometría ')
     font = pygame.font.SysFont("Ubuntu", 18, False, False)
+    imagen_robot = cargar_robot()
 
     robot = Robot()
-    robot.dibujar_robot(screen)
+    screen.fill(COLOR_BLANCO)
+    # robot.dibujar_fondo(screen)
+    robot.dibujar_robot(screen, imagen_robot)
+    enc_der, enc_izq = 0, 0
 
     run = True
     clock = pygame.time.Clock()
@@ -28,31 +31,46 @@ def main():
                 run = False
 
             elif event.type == pygame.KEYDOWN:
+                enc_der, enc_izq = 0, 0
                 # Movimiento continuo hacia delante. Tecla W.
                 if event.key == pygame.K_w:
-                    robot.movimiento_W()
+                    enc_der, enc_izq = robot.movimiento_W()
                 # Movimiento continuo hacia atrás. Tecla S.
                 if event.key == pygame.K_s:
-                    robot.movimiento_S()
+                    enc_der, enc_izq = robot.movimiento_S()
                 # Movimiento de giro sobre si mismo a la izquierda. Tecla A.
                 if event.key == pygame.K_a:
-                    robot.movimiento_A()
+                    enc_der, enc_izq = robot.movimiento_A()
                 # Movimiento de giro sobre si mismo a la derecha. Tecla D.
                 if event.key == pygame.K_d:
-                    robot.movimiento_D()
+                    enc_der, enc_izq = robot.movimiento_D()
                 # Movimiento continuo con desviación izquierda. Tecla Q.
                 if event.key == pygame.K_q:
-                    robot.movimiento_Q()
+                    enc_der, enc_izq = robot.movimiento_Q()
                 # Movimiento continuo con desviación derecha. Tecla E.
                 if event.key == pygame.K_e:
-                    robot.movimiento_E()
+                    enc_der, enc_izq = robot.movimiento_E()
+                # Tecla incorrecta = 0.
+                # robot.odo_calc(enc_der, enc_izq)  # Revisar
 
-        robot.dibujar_robot(screen)
+        # robot.dibujar_fondo(screen)
+        robot.odo_calc(enc_der, enc_izq)  # Revisar
+        robot.dibujar_robot(screen, imagen_robot)
         clock.tick(FPS)
         pygame.display.flip()
 
     pygame.quit()
 
+
+def cargar_robot():
+    """
+        Carga de la imagen del robot.
+    """
+    robot = pygame.transform.scale(pygame.image.load("assets/car_top_view.png"),
+                           (FULL_MAP_WIDTH / 10, FULL_MAP_HEIGHT / 10))
+    rotada = robot
+    rect = rotada.get_rect(center=(400, 400))
+    return robot
 
 if __name__ == '__main__':
     main()
--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+import math
 from constants import *
 
 
@@ -8,28 +9,76 @@ class Robot:
     def __init__(self):
         self.pos_x = 400
         self.pos_y = 400
-        self.rueda_der = 100
-        self.rueda_izq = 100
-        self.rueda_delantera = 80
+        self.theta_giro = 0
+        self.rotada = None
+        self.rect_rotada = None
 
-    def movimiento_W(self):
-        pass
+    @staticmethod
+    def movimiento_W():
+        encoder_der = 10
+        encoder_izq = 10
+        return encoder_der, encoder_izq
 
-    def movimiento_S(self):
-        pass
+    @staticmethod
+    def movimiento_S():
+        encoder_der = -10
+        encoder_izq = -10
+        return encoder_der, encoder_izq
 
-    def movimiento_A(self):
-        pass
+    @staticmethod
+    def movimiento_A():
+        encoder_der = 10
+        encoder_izq = -10
+        return encoder_der, encoder_izq
 
-    def movimiento_D(self):
-        pass
+    @staticmethod
+    def movimiento_D():
+        encoder_der = -10
+        encoder_izq = 10
+        return encoder_der, encoder_izq
 
-    def movimiento_Q(self):
-        pass
+    @staticmethod
+    def movimiento_Q():
+        encoder_der = 10
+        encoder_izq = 3
+        return encoder_der, encoder_izq
 
-    def movimiento_E(self):
-        pass
+    @staticmethod
+    def movimiento_E():
+        encoder_der = 3
+        encoder_izq = 10
+        return encoder_der, encoder_izq
 
-    def dibujar_robot(self, screen):
-        pass
-# window.blit(imagenrobot, pygame.)
+    def dibujar_robot(self, screen, imagen_robot):
+        """ screen.blit(imagen_robot, pygame.Rect(FULL_MAP_WIDTH, FULL_MAP_HEIGHT,
+                                              FULL_MAP_WIDTH / 10,
+                                              FULL_MAP_HEIGHT / 10)) """
+        self.rotada = pygame.transform.rotozoom(imagen_robot, math.degrees(self.theta_giro), 1)
+        self.rect_rotada = self.rotada.get_rect(center=(self.pos_x, self.pos_y))
+        screen.blit(self.rotada, self.rect_rotada)
+        # screen.blit(imagen_robot, (self.pos_x, self.pos_y))
+        pygame.display.flip()
+
+    def dibujar_fondo(self, screen):
+        screen.fill(COLOR_BLANCO)
+        pygame.display.flip()
+
+    def odo_calc(self, vel_encoder_der, vel_encoder_izq):
+        conversion_der = (2 * math.pi * (DIAM_RUEDA_DER / 2)) / RESOLUCION_ENCODER
+        conversion_izq = (2 * math.pi * (DIAM_RUEDA_IZQ / 2)) / RESOLUCION_ENCODER
+        rueda_der = conversion_der * vel_encoder_der
+        rueda_izq = conversion_izq * vel_encoder_izq
+
+        if rueda_der < 0 and rueda_izq > 0:
+            distancia_recorrida = rueda_izq / 2
+        elif rueda_izq < 0 and rueda_der > 0:
+            distancia_recorrida = rueda_der / 2
+        else:
+            distancia_recorrida = (rueda_der + rueda_izq) / 2
+        giro_recorrido = -((rueda_der - rueda_izq) / SEPARACION_RUEDAS)  # Revisar
+
+        print(vel_encoder_der, vel_encoder_izq, rueda_der, rueda_izq, distancia_recorrida, giro_recorrido)
+
+        self.theta_giro = self.theta_giro + giro_recorrido
+        self.pos_x = self.pos_x + distancia_recorrida * math.cos(self.theta_giro)
+        self.pos_y = self.pos_y + distancia_recorrida * math.sin(self.theta_giro)
