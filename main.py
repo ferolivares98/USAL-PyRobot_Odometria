dd7f1d00c280ad3f483286e0e77fbbe8ca25d449
ferolivares98 <ferolinaranjo@gmail.com>
2022-04-13 21:37:14 +0200

    V1.1 Controles avanzados

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from robot import *
-
+from constants import *
 
 
 # https://gm0.org/en/latest/docs/software/odometry.html
@@ -9,7 +9,7 @@ from robot import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((FULL_MAP_WIDTH + CON_WIDTH,
+    screen = pygame.display.set_mode((FULL_MAP_WIDTH + EXTRA_WIDTH,
                                       FULL_MAP_HEIGHT))
     pygame.display.set_caption(' Robot  |  Odometría ')
     imagen_robot = cargar_robot()
@@ -18,6 +18,7 @@ def main():
     screen.fill(COLOR_BLANCO)
     robot.dibujar_robot(screen, imagen_robot)
     enc_der, enc_izq = 0, 0
+    last_enc_der, last_enc_izq = 0, 0
 
     run = True
     clock = pygame.time.Clock()
@@ -35,24 +36,46 @@ def main():
                 if event.key == pygame.K_w:
                     enc_der, enc_izq = robot.movimiento_W()
                 # Movimiento continuo hacia atrás. Tecla S.
-                if event.key == pygame.K_s:
+                elif event.key == pygame.K_s:
                     enc_der, enc_izq = robot.movimiento_S()
                 # Movimiento de giro sobre si mismo a la izquierda. Tecla A.
-                if event.key == pygame.K_a:
+                elif event.key == pygame.K_a:
                     enc_der, enc_izq = robot.movimiento_A()
                 # Movimiento de giro sobre si mismo a la derecha. Tecla D.
-                if event.key == pygame.K_d:
+                elif event.key == pygame.K_d:
                     enc_der, enc_izq = robot.movimiento_D()
                 # Movimiento continuo con desviación izquierda. Tecla Q.
-                if event.key == pygame.K_q:
+                elif event.key == pygame.K_q:
                     enc_der, enc_izq = robot.movimiento_Q()
                 # Movimiento continuo con desviación derecha. Tecla E.
-                if event.key == pygame.K_e:
+                elif event.key == pygame.K_e:
                     enc_der, enc_izq = robot.movimiento_E()
+                # Resbalón de la rueda derecha. Tecla V.
+                elif event.key == pygame.K_v:
+                    robot.resbalon_derecha()
+                # Resbalón de la rueda izquierda. Tecla B.
+                elif event.key == pygame.K_b:
+                    robot.resbalon_izquierda()
+                # Reinicio de las variables.
+                elif event.key == pygame.K_r:
+                    robot.reinicio()
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_i] and keys[pygame.K_UP]:
+                    robot.encoder_aumenta()
+                elif keys[pygame.K_i] and keys[pygame.K_DOWN]:
+                    robot.encoder_disminuye()
+                elif keys[pygame.K_o] and keys[pygame.K_UP]:
+                    robot.diametro_derecha_aumenta()
+                elif keys[pygame.K_o] and keys[pygame.K_DOWN]:
+                    robot.diametro_derecha_disminuye()
+                elif keys[pygame.K_p] and keys[pygame.K_UP]:
+                    robot.diametro_izquierda_aumenta()
+                elif keys[pygame.K_p] and keys[pygame.K_DOWN]:
+                    robot.diametro_izquierda_disminuye()
                 # Tecla incorrecta = 0.
             # robot.odo_calc(enc_der, enc_izq, ticks_vel) #Activa aceleración continuada (Recoge todos los eventos)
         # robot.dibujar_fondo(screen)
-        ticks_vel = (pygame.time.get_ticks() - last_time)/1000
+        ticks_vel = (pygame.time.get_ticks() - last_time) / 1000
         last_time = pygame.time.get_ticks()
         pygame.display.flip()
         robot.dibujar_fondo(screen)
--- robot.py
+++ robot.py
@@ -12,15 +12,25 @@ class Robot:
         self.theta_giro = 0
         self.rueda_der = 0
         self.rueda_izq = 0
+        self.diametro_der = DIAM_RUEDA_DER
+        self.diametro_izq = DIAM_RUEDA_IZQ
+        self.encoder = RESOLUCION_ENCODER
+        self.resbalon = 0
 
         self.rotada = None
         self.rect_rotada = None
         self.trail_list = []
 
         self.fuente = pygame.font.SysFont("Arial", 24)
+        self.fuenteInstrucciones = pygame.font.SysFont("Arial", 18)
         self.texto = self.fuente.render('default', True, COLOR_NEGRO, COLOR_BLANCO)
         self.textRect = self.texto.get_rect()
         self.textRect.center = (FULL_MAP_WIDTH / 10, FULL_MAP_HEIGHT / 10)
+        self.texto_ruedas = self.fuente.render('default', True, COLOR_NEGRO, COLOR_BLANCO)
+        self.textRect_ruedas = self.texto_ruedas.get_rect()
+        self.textRect_ruedas.center = (FULL_MAP_WIDTH / 10, FULL_MAP_HEIGHT / 10 + self.texto_ruedas.get_height())
+        self.instrucciones = self.fuenteInstrucciones.render('default', True, COLOR_BLANCO, COLOR_NEGRO)
+        self.instruccionesRect = self.instrucciones.get_rect()
 
     @staticmethod
     def movimiento_W():
@@ -58,6 +68,44 @@ class Robot:
         encoder_izq = 10
         return encoder_der, encoder_izq
 
+    def encoder_aumenta(self):
+        self.encoder += 10
+
+    def encoder_disminuye(self):
+        if self.encoder > 10:
+            self.encoder -= 10
+
+    def diametro_derecha_aumenta(self):
+        self.diametro_der += 1
+
+    def diametro_izquierda_aumenta(self):
+        self.diametro_izq += 1
+
+    def diametro_derecha_disminuye(self):
+        if self.diametro_der > 10:
+            self.diametro_der -= 1
+
+    def diametro_izquierda_disminuye(self):
+        if self.diametro_izq > 10:
+            self.diametro_izq -= 1
+
+    def resbalon_derecha(self):
+        self.resbalon = -40
+
+    def resbalon_izquierda(self):
+        self.resbalon = 40
+
+    def reinicio(self):
+        self.pos_x = 400
+        self.pos_y = 400
+        self.theta_giro = 0
+        self.rueda_der = 0
+        self.rueda_izq = 0
+        self.diametro_der = DIAM_RUEDA_DER
+        self.diametro_izq = DIAM_RUEDA_IZQ
+        self.encoder = RESOLUCION_ENCODER
+        self.resbalon = 0
+
     def dibujar_robot(self, screen, imagen_robot):
         """ screen.blit(imagen_robot, pygame.Rect(FULL_MAP_WIDTH, FULL_MAP_HEIGHT,
                                               FULL_MAP_WIDTH / 10,
@@ -68,14 +116,34 @@ class Robot:
 
     def dibujar_fondo(self, screen):
         screen.fill(COLOR_BLANCO)
+        pygame.draw.rect(screen, COLOR_NEGRO, (FULL_MAP_WIDTH, 0, EXTRA_WIDTH, EXTRA_HEIGHT))
+        inst = ("Controles:\n- Avance -> W\n- Retroceso -> S\n"
+                "- 90ºIzquierda -> A\n- 90ºDerecha -> D\n"
+                "- LigeroGiroIzq -> Q\n- LigeroGiroDer -> E\n- Parar -> \"Resto no en uso\"\n\n"
+                "- AumentarEncoder (+10) \n     I + \u2191\n- DisminuirEncoder (-10)\n     I + \u2193\n"
+                "- AumentarRuedaDer (+1)\n     O + \u2191\n- DisminuirRuedaDer (-1)\n    O + \u2193\n"
+                "- AumentarRuedaIzq (+1)\n     P + \u2191\n- DisminuirRuedaIzq (-1)\n    P + \u2193\n\n"
+                "- ResbalónRuedaDer -> V\n- ResbalónRuedaIzq -> B\n- ReiniciarValores -> R")
+        inst_list = inst.splitlines()
+        tam_height = self.instrucciones.get_size()
+        pos_inst_height = FULL_MAP_HEIGHT / 60
+        for i, l in enumerate(inst_list):
+            self.instrucciones = self.fuenteInstrucciones.render(l, True, COLOR_BLANCO, COLOR_NEGRO)
+            screen.blit(self.instrucciones,
+                        ((FULL_MAP_WIDTH + EXTRA_WIDTH - (EXTRA_WIDTH / 1.05)), pos_inst_height))
+            pos_inst_height += tam_height[1] + (tam_height[1] / 2)
 
     def dibujar_pos_info(self, screen):
-        texto = f"PosX = {int(self.pos_x)} | PosY = {int(self.pos_y)} | " \
-                f"Theta = {int(math.degrees(int(self.theta_giro)))} || " \
-                f"Vel_Rueda_Der = {int(self.rueda_der)} | " \
-                f"Vel_rueda_Izq = {int(self.rueda_izq)}"
-        self.texto = self.fuente.render(texto, True, COLOR_NEGRO, COLOR_BLANCO)
+        text = f"PosX = {int(self.pos_x)} | PosY = {int(self.pos_y)} | " \
+               f"Theta = {int(math.degrees(int(self.theta_giro)))} || " \
+               f"Vel_Rueda_Der = {int(self.rueda_der)} | " \
+               f"Vel_rueda_Izq = {int(self.rueda_izq)}"
+        self.texto = self.fuente.render(text, True, COLOR_NEGRO, COLOR_BLANCO)
         screen.blit(self.texto, self.textRect)
+        text2 = f"Tam_Rueda_Der = {int(self.diametro_der)} | Tam_Rueda_Izq = {int(self.diametro_izq)} | " \
+               f"Encoder = {int(self.encoder)}"
+        self.texto_ruedas = self.fuente.render(text2, True, COLOR_NEGRO, COLOR_BLANCO)
+        screen.blit(self.texto_ruedas, self.textRect_ruedas)
 
     def dibujar_trail(self, screen):
         for i in range(0, len(self.trail_list) - 1):
@@ -84,11 +152,28 @@ class Robot:
         self.trail_list.append((self.pos_x, self.pos_y))
 
     def odo_calc(self, vel_encoder_der, vel_encoder_izq, ticks_vel):
-        conversion_der = (2 * math.pi * (DIAM_RUEDA_DER / 2)) / RESOLUCION_ENCODER
-        conversion_izq = (2 * math.pi * (DIAM_RUEDA_IZQ / 2)) / RESOLUCION_ENCODER
-        self.rueda_der = conversion_der * vel_encoder_der
-        self.rueda_izq = conversion_izq * vel_encoder_izq
+        conversion_der = (2 * math.pi * (self.diametro_der / 2)) / self.encoder
+        conversion_izq = (2 * math.pi * (self.diametro_izq / 2)) / self.encoder
+        '''
+        Dependiendo del valor de resbalón, no sucede nada o se resbala durante 200 llamadas
+        la rueda correspondiente al valor de la variable. Si es negativa la derecha, si es positiva
+        la izquierda.
+        '''
+        if self.resbalon == 0:
+            self.rueda_der = conversion_der * vel_encoder_der
+            self.rueda_izq = conversion_izq * vel_encoder_izq
+        elif self.resbalon < 0:  # Resbalon de la rueda derecha.
+            self.rueda_der = (conversion_der * vel_encoder_der) - 30
+            self.rueda_izq = conversion_izq * vel_encoder_izq
+            self.resbalon += 1
+        else:  # Resbalón de la rueda izquierda.
+            self.rueda_der = conversion_der * vel_encoder_der
+            self.rueda_izq = (conversion_izq * vel_encoder_izq) - 30
+            self.resbalon -= 1
 
+        self.pos_x += ((self.rueda_der + self.rueda_izq) / 2) * math.cos(self.theta_giro) * ticks_vel
+        self.pos_y -= ((self.rueda_der + self.rueda_izq) / 2) * math.sin(self.theta_giro) * ticks_vel
+        self.theta_giro += (self.rueda_der - self.rueda_izq) / SEPARACION_RUEDAS * ticks_vel
         """
         Funcional pero no corresponde con el sprite del robot-vehículo.
         if rueda_der < 0 and rueda_izq > 0:
@@ -103,7 +188,5 @@ class Robot:
 
         self.theta_giro = self.theta_giro + giro_recorrido
         self.pos_x = self.pos_x + distancia_recorrida * math.cos(self.theta_giro)
-        self.pos_y = self.pos_y + distancia_recorrida * math.sin(self.theta_giro)"""
-        self.pos_x += ((self.rueda_der + self.rueda_izq) / 2) * math.cos(self.theta_giro) * ticks_vel
-        self.pos_y -= ((self.rueda_der + self.rueda_izq) / 2) * math.sin(self.theta_giro) * ticks_vel
-        self.theta_giro += (self.rueda_der - self.rueda_izq) / SEPARACION_RUEDAS * ticks_vel
+        self.pos_y = self.pos_y + distancia_recorrida * math.sin(self.theta_giro)
+        """
